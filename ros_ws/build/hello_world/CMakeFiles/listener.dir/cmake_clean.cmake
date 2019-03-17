8ef3462073f39e3ae855aefb42e89accdb19d5cc
WANGZIHAO <rekindle@sjtu.edu.cn>
2019-03-18 01:33:00 +0800

    #	new file:   ros_ws/build/CMakeFiles/3.5.1/CMakeCCompiler.cmake

---
+++ ros_ws/build/hello_world/CMakeFiles/listener.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/listener.dir/src/listener.cpp.o"
+  "/home/zhwwwwang/ros_ws/devel/lib/hello_world/listener.pdb"
+  "/home/zhwwwwang/ros_ws/devel/lib/hello_world/listener"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/listener.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
